20c0afb4177f8e204d968d26120479de0dba4747
yank0vy3rdna <yankovyerdna@yandex.ru>
2021-04-20 14:54:09 +0300

    feat: env

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
     public static void main(String[] args) {
         Result result = new Result(1.0,1.0,2.0);
-        result.checkTheArea();f
+        result.checkTheArea();
         System.out.println(result.getResult());
     }
 }
